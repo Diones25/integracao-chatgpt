a8df199cd84ad1f514b546009645e17fe49972ab
Diones <pereiradiones987@gmail.com>
2025-03-13 20:14:12 -0300

    criando service

--- src/chat-gpt/chat-gpt.service.ts
+++ src/chat-gpt/chat-gpt.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import { max } from 'rxjs';
 
 @Injectable()
-export class ChatGptService {}
+export class ChatGptService {
+  private readonly apiKey: string | undefined;
+  private readonly apiUrl: string = 'https://api.openai.com/v1/chat/completions';
+
+  constructor(private configService: ConfigService) {
+    this.apiKey = this.configService.get<string>('OPENAI_API_KEY');
+  }
+
+  async generateResponse(prompt: string) {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.apiKey}`,
+    };
+
+    const data = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: prompt }],
+      max_tokens: 150,
+    }
+
+    try {
+      const response = await axios.post(this.apiUrl, data, { headers });
+      return response.data.choices[0].message.content.trim();
+    } catch (error) {
+      console.error('Error calling OpenAI API:', error.response?.data || error.message);
+      throw new BadRequestException('Failed to generate response from ChatGPT');
+    }
+  }
+}
