c1dcbc9e612f0a170c837b6a841d054c36412053
Diones <pereiradiones987@gmail.com>
2025-03-13 20:04:51 -0300

    criando modulo, service e controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ChatGptModule } from './chat-gpt/chat-gpt.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    ChatGptModule,
+    
   ],
   controllers: [],
   providers: [],
