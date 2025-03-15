4a4a27a8606e5a6549f413662b1e120502bd3d75
Diones <pereiradiones987@gmail.com>
2025-03-14 23:14:35 -0300

    anotação do swagger no DTO

--- src/chat-gpt/dtos/prompt-chat-gtp.dto.ts
+++ src/chat-gpt/dtos/prompt-chat-gtp.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class PromptChatGtpDto {
   
+  @ApiProperty({ example: 'Qual a capital da Alemanha', description: 'Prompt para o ChatGPT' })
   @IsString({ message: 'Deve ser uma string' })
   prompt: string;
 }
